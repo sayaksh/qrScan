78aea0c40d92e82709bb0d1d3900a9a5645363ba
sayaksh <kshsayadevi@gmail.com>
2022-08-12 15:43:31 +0530

    QR scan ok but litle prob to display, wait

---
+++ android/app/src/main/kotlin/com/example/qr_scanning/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.qr_scanning
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: ElevatedButton(
+                onPressed: () async {
+                  String s = await FlutterBarcodeScanner.scanBarcode(
+                      "#ff6666", "cancel", true, ScanMode.QR);
+                  Text(s);
+                },
+                child: const Text("click")),
+          )
+        ],
+      ),
+    );
+  }
+}
